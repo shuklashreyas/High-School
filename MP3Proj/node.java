cf0b0f3ca756538e31c48493c01726ae53d2b214
shreyman21 <66776238+shreyman21@users.noreply.github.com>
2021-08-04 00:28:43 +0800

    Create node.java

---
+++ MP3Proj/node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shreyasshukla
+ */
+public class node 
+{
+    public song data;
+    public node next;
+    
+    
+    public node(song d)
+    {
+        data = d;
+
+    }
+
+    
+}
