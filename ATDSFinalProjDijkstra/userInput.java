fd9de476445bd25a8b57aaa2030d9d5de2a4685d
shreyman21 <66776238+shreyman21@users.noreply.github.com>
2021-08-03 23:05:18 +0800

    Create userInput.java

---
+++ ATDSFinalProjDijkstra/userInput.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author shreyasshukla
+ */
+public class userInput 
+{
+    private int source;
+    private int destination;
+
+    public userInput(int source, int destination) 
+    {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public int getSource() 
+    {
+        return source;
+    }
+
+    public int getDestination() 
+    {
+        return destination;
+    }
+
+    public void setSource(int source) 
+    {
+        this.source = source;
+    }
+
+    public void setDestination(int destination) 
+    {
+        this.destination = destination;
+    }
+    
+    
+    
+    
+}
