f1bf181e1e1b38cf2495d093f43a34950575f5c3
shreyman21 <66776238+shreyman21@users.noreply.github.com>
2021-08-03 23:21:34 +0800

    Create Element.java

---
+++ balancingChemEq/Element.java
@@ -0,0 +1,18 @@
+public class Element
+{
+
+   private String name;
+   private int charge;
+   
+   public Element(String name,int charge)
+   {
+      this.name = name;
+      this.charge = charge;
+   }
+
+   public String getName()
+   {  
+      return name;
+   }
+
+}
