e0b11af5fb402d416b8565e61b7a8b7941b0d160
shreyman21 <66776238+shreyman21@users.noreply.github.com>
2021-08-03 23:07:40 +0800

    Create location.java

---
+++ ATDSFinalProjDijkstra/location.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author shreyasshukla
+ */
+public class location 
+{
+    private int id;
+    private String name;
+
+    public location(int id, String name) 
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+    
+    
+    
+    
+    
+}
