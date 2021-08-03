9870d7012aff1e27db4d3005d3c6cde0c063ad80
shreyman21 <66776238+shreyman21@users.noreply.github.com>
2021-08-03 23:42:19 +0800

    Create hello.c

---
+++ CS50/pset1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+   string name = get_string("What is your name?\n");
+   printf("hello, %s\n", name);
+}
